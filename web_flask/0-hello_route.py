ca081ee9e7aed51d9029463cd2c62d88c177eb8b
Margret Andalyha <125597880+margret739@users.noreply.github.com>
2024-06-27 00:11:15 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 #define the route for the root URL. '/'
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-	""" Displays 'HELLO HBNB!"""
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def index():
+	""" Displays HELLO HBNB!"""
 	return "Hello HBNB!"
 
 if __name__ == "__main__":
 	# Start the flask development server
 	# Listen on all available interface networks (0.0.0.0) and port 5000
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port='5000')
