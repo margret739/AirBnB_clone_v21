a481fe85d18c9f81e66219ae3f62b86f2d5ddaac
Margret Andalyha <125597880+margret739@users.noreply.github.com>
2024-06-27 00:12:59 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,4 +14,4 @@ def hello_hbnb():
 if __name__ == "__main__":
 	# Start the flask development server
 	# Listen on all available interface networks (0.0.0.0) and port 5000
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port='5000')
